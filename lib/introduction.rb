331c109fce5a66055400c0473df65e027f724b2d
Anthony mendoza <anthonymendoza9922@gmail.com>
2020-02-26 17:45:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,9 +7,8 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 def introduction_with_language_optional(name, language)
-puts "Hi, my name is #{name} and I am learning to program in Ruby."
-
- puts "Hi, my name is #{name} and I am learning to program in #{language}."
+program = "ruby"
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
   
